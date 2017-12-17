69746f7774de535a7c1fbe022bf87f814b59893d
gabikaSlani <gabika.slani@gmail.com>
2017-12-17 20:04:38 +0100

    Revert "archiv,preteky,novy pretek - kopia"

--- source/novy_pretek.php
+++ source/novy_pretek.php
@@ -24,49 +24,10 @@ else{
 
 ?>
 <!DOCTYPE HTML>
+
 <html>
 <?php
   hlavicka("Nový pretek");
-  if(isset($_GET['novy']) && isset ($_GET['id'])){
-      $po = new PRETEKY();
-      $po = PRETEKY::vrat_pretek($_GET["id"]);
-      ?>
-      <section id="uprav_preteky">
-      <form method="post" enctype="multipart/form-data">
-        <table>
-         <?php if(isset($_POST['nazov']) && !over($_POST['nazov'])){echo'<tr><td><font color="red">Nevyplnili ste názov!</font></td></tr>';} ?>
-         <tr>
-          <td><label for="nazov">Názov pretekov</label></td>
-          <td><input type="text" name="nazov" id="nazov" size="30" value=""></td>
-        </tr>
-        <?php if(isset($_POST['datum']) && !over($_POST['datum'])){echo'<tr><td><font color="red">Nevyplnili ste dátum!</font></td></tr>';} ?>
-        <tr>
-          <td><label for="datetimepicker">Dátum konania</label>  </td>
-          <td><input type="text" name="datum" id="datetimepicker" size="30" value=""></td>
-        </tr>
-        <?php if(isset($_POST['deadline']) && !over($_POST['deadline'])){echo'<tr><td><font color="red">Nevyplnili ste deadline!</font></td></tr>';} ?>
-        <tr>
-          <td><label for="datetimepicker1">Deadline prihlásenia</label></td>
-          <td><input type="text" name="deadline" id="datetimepicker1" size="30" value=""></td>
-        </tr>
-        <tr>
-          <td> <label for="poznamka">Poznámka</label> </td>
-          <td> <textarea cols="80" rows="15" name="poznamka" id="poznamka"><?php if(isset($_POST['poznamka'])){echo $_POST['poznamka'];}else{echo $po->POZNAMKA;} ?></textarea></td>
-        </tr>
-      </table>
-      <table><?php
-        if(isset($_POST['posli'])&&!isset($_POST['incharge'])){
-          echo'<tr><td><font color="red">Musíte zadať aspoň jednu kategóriu!</font></td></tr>';
-        }
-        PRETEKY::vypis_zoznam_pretek_table(); PRETEKY::vypis_zoznam_ostatne_table(); ?></table>
-        <p id="buttons">
-      <input type="submit" name="vytvor" value="Vytvor">
-    </p>
-  </form>
-</section>
-<?php
-  }
-else{
 ?>
 <section id="uprav_preteky">
 	<form method="post" enctype="multipart/form-data">
@@ -103,9 +64,6 @@ else{
     </p>
   </form>
 </section>
-<?php 
-} 
-?>
 
 <script src="js/jquery.js"></script>
 <script src="js/jquery.datetimepicker.js"></script>
