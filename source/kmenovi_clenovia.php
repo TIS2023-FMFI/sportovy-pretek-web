c4f7e8a25c2b875b423e77b3e4ccc6107ff5e6eb
David Krchňavý <david.krchnavy@gmail.com>
2023-12-07 20:31:43 +0100

    kmenovi_clenovia.php: reformat and fix some warnings

--- source/kmenovi_clenovia.php
+++ source/kmenovi_clenovia.php
@@ -1,50 +1,41 @@
-      
 <?php
 session_start();
 include('funkcie.php');
 include('pouzivatelia.php');
 include('preteky.php');
 
-if(isset($_POST['export'])){
+if (isset($_POST['export'])) {
     PRETEKY::exportujK();
 }
 ?>
 
 <!DOCTYPE HTML>
-<html>
+<html lang="sk">
 <?php
 hlavicka("Kmeňoví členovia");
-if(isset($_POST["vymaz"])){
-    if(vymaz_clena($_POST["id_clen"])){
+if (isset($_POST["vymaz"])) {
+    if (vymaz_clena($_POST["id_clen"])) {
         echo "<h4 align='center'>Člen bol odstrátený zo zoznamu kmeňových členov.</h4>";
     }
 }
 
 
+if (!empty($_POST['id']) && !empty($_POST['obsah']) && !empty($_POST['stlpec'])) {
+    $id = $_POST['id'];
+    $obsah = $_POST['obsah'];
+    $stlpec = $_POST['stlpec'];
 
-if ((isset($_POST['id'])) && !empty($_POST['id']) && (isset($_POST['obsah'])) && !empty($_POST['obsah']) && (isset($_POST['stlpec'])) && !empty($_POST['stlpec'])) {
-  $id = $_POST['id'];
-  $obsah = $_POST['obsah'];
-  $stlpec = $_POST['stlpec'];
-
-  echo uprav($id, $obsah, $stlpec);
+    echo uprav($id, $obsah, $stlpec);
 }
-
 
 
 vypis_kmenovych_clenov();
 
 ?>
 <form method="post">
-<input type="submit" name="export" value="Exportuj zoznam">
+    <input type="submit" name="export" value="Exportuj zoznam">
 </form>
-<?php
-
-
-paticka();
-?>
-
-
+<?php paticka(); ?>
 </html>
 
 
@@ -54,97 +45,74 @@ $obsah = "";
 $stlpec = "";
 $id = "";
 
-function vypis_kmenovych_clenov(){
-
-        ?>
-        <div>
-        <br>
-        <table style="width:100%;" border=1 class="tabulkaVykonou tablesorter" id="kmenovi">
+function vypis_kmenovych_clenov()
+{ ?>
+    <div>
+    <br>
+    <table style="width:100%;" class="tabulkaVykonou tablesorter" id="kmenovi">
         <thead>
         <tr>
-                <th class="prvy header"></th>
-                <th class="prvy header">Meno</th>
-                <th class="prvy header">Priezisko</th>
-                <th class="prvy header">Pohlavie</th>
-                <th class="prvy header">Dátum narodenia</th>
-                <th class="prvy header">Krajina narodenia</th>
-                <th class="prvy header">Štátna príslušnosť</th>
-                <th class="prvy header">Krajina trvalého bydliska</th>
-                <th class="prvy header">Ulica</th>
-                <th class="prvy header">Číslo domu</th>
-                <th class="prvy header">PSČ</th>
-                <th class="prvy header">Mesto</th>
-                <th class="prvy header">Telefón</th>
-                <th class="prvy header">Mail</th>
-                <th class="prvy header">Číslo čipu</th>
-                <th class="prvy header">Registračné číslo</th>
-                <th class="prvy header"></th>
-            </tr>
+            <th class="prvy header"></th>
+            <th class="prvy header">Meno</th>
+            <th class="prvy header">Priezisko</th>
+            <th class="prvy header">Pohlavie</th>
+            <th class="prvy header">Dátum narodenia</th>
+            <th class="prvy header">Krajina narodenia</th>
+            <th class="prvy header">Štátna príslušnosť</th>
+            <th class="prvy header">Krajina trvalého bydliska</th>
+            <th class="prvy header">Ulica</th>
+            <th class="prvy header">Číslo domu</th>
+            <th class="prvy header">PSČ</th>
+            <th class="prvy header">Mesto</th>
+            <th class="prvy header">Telefón</th>
+            <th class="prvy header">Mail</th>
+            <th class="prvy header">Číslo čipu</th>
+            <th class="prvy header">Registračné číslo</th>
+            <th class="prvy header"></th>
+        </tr>
         </thead>
-    <?php
-    $db = napoj_db();
-    $sql =<<<EOF
-         SELECT * from Kmenovi_clenovia AS k JOIN Pouzivatelia AS p ON p.id_kmen_clen = k.id ORDER BY p.priezvisko ASC;
+        <?php
+        $db = napoj_db();
+        $sql = <<<EOF
+         SELECT * from Kmenovi_clenovia AS k JOIN Pouzivatelia AS p ON p.id_kmen_clen = k.id ORDER BY p.priezvisko;
 EOF;
-    $ret = $db->query($sql);
-     
-    while($row = $ret->fetchArray(SQLITE3_ASSOC) ){
-    $cesta_obrazok = vrat_cestu_obrazka($row['id']);
-    $id_kmen = $row['id_kmen_clen'];
-    $datum = 'priezvisko';
+        $ret = $db->query($sql);
 
-    echo "<tr>";
-            echo "<td><span class='tooltiptext'><img src='".$cesta_obrazok."' alt='fotka' height='400' width='450'></span><a class='fntb' href='profil.php?id=".$row['id']."'>Profil</a></td>";
+        while ($row = $ret->fetchArray(SQLITE3_ASSOC)) {
+            $cesta_obrazok = vrat_cestu_obrazka($row['id']);
+            echo "<tr>";
+            echo "<td><span class='tooltiptext'><img src='" . $cesta_obrazok . "' alt='fotka' height='400' width='450'></span><a class='fntb' href='profil.php?id=" . $row['id'] . "'>Profil</a></td>";
+            echo "<td contenteditable name='meno' id='meno" . $row['id'] . "' onkeyup='save2(event, this," . $row['id'] . ")'>" . $row['meno'] . "</td>";
+            echo "<td contenteditable name='priezvisko' id='priezvisko" . $row['id'] . "' onkeyup='save2(event, this," . $row['id'] . ")'>" . $row['priezvisko'] . "</td>";
+            echo "<td contenteditable name='pohlavie' id='pohlavie" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['pohlavie'] . "</td>";
+            echo "<td contenteditable name='datum_narodenia' id='datum_narodenia" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['datum_narodenia'] . "</td>";
+            echo "<td contenteditable name='krajina_narodenia' id='krajina_narodenia" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['krajina_narodenia'] . "</td>";
+            echo "<td contenteditable name='statna_prislusnost' id='statna_prislusnost" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['statna_prislusnost'] . "</td>";
+            echo "<td contenteditable name='krajina_trvaleho_pobytu' id='krajina_trvaleho_pobytu" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['krajina_trvaleho_pobytu'] . "</td>";
+            echo "<td contenteditable name='ulica' id='ulica" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['ulica'] . "</td>";
+            echo "<td contenteditable name='cislo_domu' id='cislo_domu" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['cislo_domu'] . "</td>";
+            echo "<td contenteditable name='psc' id='psc" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['psc'] . "</td>";
+            echo "<td contenteditable name='mesto' id='mesto" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['mesto'] . "</td>";
+            echo "<td contenteditable name='telefon' id='telefon" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['telefon'] . "</td>";
+            echo "<td contenteditable name='mail' id='mail" . $row['id'] . "' onkeyup='save2(event, this," . $row['id_kmen_clen'] . ")'>" . $row['mail'] . "</td>";
+            echo "<td contenteditable name='cip' id='cip" . $row['id'] . "' onkeyup='save2(event, this," . $row['id'] . ")'>" . $row['cip'] . "</td>";
+            echo "<td contenteditable name='os_i_c' id='os_i_c" . $row['id'] . "' onkeyup='save2(event, this," . $row['id'] . ")'>" . $row['os_i_c'] . "</td>";
+            echo "<td><form method='post'><input type='hidden' name='id_clen' value='" . $row['id_kmen_clen'] . "'><input type='submit' name='vymaz' value='Vymaž'></form></td>";
+            echo "</tr>";
+        }
+        // echo "Operation done successfully"."<br>";   ///////////////////
+        $db->exec($sql);
+        $db->close();
 
-            echo "<td contenteditable name='meno' id='meno".$row['id']."' onkeyup='save2(event, this,".$row['id'].")'>".$row['meno']."</td>";
-           
-            echo "<td contenteditable name='priezvisko' id='priezvisko".$row['id']."' onkeyup='save2(event, this,".$row['id'].")'>".$row['priezvisko']."</td>";
-            
-            echo "<td contenteditable name='pohlavie' id='pohlavie".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['pohlavie']."</td>";
-            
-            echo "<td contenteditable name='datum_narodenia' id='datum_narodenia".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['datum_narodenia']."</td>";
-            
-            echo "<td contenteditable name='krajina_narodenia' id='krajina_narodenia".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['krajina_narodenia']."</td>";
-            
-            echo "<td contenteditable name='statna_prislusnost' id='statna_prislusnost".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['statna_prislusnost']."</td>";
-            
-            echo "<td contenteditable name='krajina_trvaleho_pobytu' id='krajina_trvaleho_pobytu".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['krajina_trvaleho_pobytu']."</td>";
-            
-            echo "<td contenteditable name='ulica' id='ulica".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['ulica']."</td>";
-            
-            echo "<td contenteditable name='cislo_domu' id='cislo_domu".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['cislo_domu']."</td>";
-            
-            echo "<td contenteditable name='psc' id='psc".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['psc']."</td>";
-            
-            echo "<td contenteditable name='mesto' id='mesto".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['mesto']."</td>";
-            
-            echo "<td contenteditable name='telefon' id='telefon".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['telefon']."</td>";
-            
-            echo "<td contenteditable name='mail' id='mail".$row['id']."' onkeyup='save2(event, this,".$row['id_kmen_clen'].")'>".$row['mail']."</td>";
-            
-            echo "<td contenteditable name='cip' id='cip".$row['id']."' onkeyup='save2(event, this,".$row['id'].")'>".$row['cip']."</td>";
-            
-            echo "<td contenteditable name='os_i_c' id='os_i_c".$row['id']."' onkeyup='save2(event, this,".$row['id'].")'>".$row['os_i_c']."</td>";
-            
-            echo "<td><form method='post'><input type='hidden' name='id_clen' value='".$row['id_kmen_clen']."'><input type='submit' name='vymaz' value='Vymaž'></form></td>";
-    echo "</tr>";
-    
-    } 
-    // echo "Operation done successfully"."<br>";   ///////////////////
-    $db->exec($sql);
-    $db->close();
-    
-    ?>
+        ?>
 
-        </table>
+    </table>
 
-        </div><?php
+    </div>
+<?php }
 
-        return;
-
-}
-
-function vymaz_clena($id){
+function vymaz_clena($id)
+{
     $db = napoj_db();
 
     if ($db) {
@@ -164,45 +132,30 @@ EOF;
 
 }
 
-
-
-
-function uprav($id, $obsah, $stlpec){
-  $db = napoj_db();
-  if ($stlpec == "meno" || $stlpec == "priezvisko" || $stlpec == "os_i_c" || $stlpec == "cip"){
-    $sql = <<<EOF
+function uprav($id, $obsah, $stlpec)
+{
+    $db = napoj_db();
+    if ($stlpec == "meno" || $stlpec == "priezvisko" || $stlpec == "os_i_c" || $stlpec == "cip") {
+        $sql = <<<EOF
           UPDATE Pouzivatelia SET $stlpec = '$obsah' WHERE id = $id; 
 EOF;
-  } 
-  else{
-    $sql = <<<EOF
+    } else {
+        $sql = <<<EOF
           UPDATE Kmenovi_clenovia SET $stlpec = '$obsah' WHERE id = $id; 
 EOF;
-  }
-  if ($db) {
-        
+    }
+    if ($db) {
+
         $db->exec($sql);
         $db->close();
-  }
-  return true;
-       
-}
+    }
+    return true;
 
-?>
-<script
-        src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha256-k2WSCIexGzOj3Euiig+TlR8gA0EmPjuc79OEeY5L45g="
-        crossorigin="anonymous">
-            
-
-</script>
-<script type="text/javascript"></script>
+} ?>
 
 <script type="text/javascript" src="sorter/jquery.tablesorter.js"></script>
 <script>
-    $(document).ready(function(){
+    $(document).ready(function () {
         $("#kmenovi").tablesorter({dateFormat: "uk"});
-        });
+    });
 </script>
-
-
