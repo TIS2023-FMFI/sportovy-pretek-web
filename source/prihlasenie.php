96fbffce74145d9b7034bddc6a0f30f3a325acc2
David Krchňavý <david.krchnavy@gmail.com>
2023-12-07 21:17:35 +0100

    prihlasenie.php: reformat code and fix some warnings

--- source/prihlasenie.php
+++ source/prihlasenie.php
@@ -6,45 +6,45 @@ session_start();
 ?>
 
 <!DOCTYPE HTML>
-<html>
+<html lang="sk">
 <?php
 hlavicka("Prihlásenie administrátora");
 
 if (isset($_POST['ajax_heslo'])) {
-   posli_heslo($heslo, $mail_od, $mail_komu);
+    posli_heslo($heslo, $mail_od, $mail_komu);
 }
 
-if (isset($_POST['heslo'])&&$_POST['heslo']==$heslo){
-  $_SESSION['admin']=1;
-  echo "<META http-equiv='refresh' content='0;URL=index.php'>";
+if (isset($_POST['heslo']) && $_POST['heslo'] == $heslo) {
+    $_SESSION['admin'] = 1;
+    echo "<META http-equiv='refresh' content='0;URL=index.php'>";
+} else {
+    ?>
+    <form method="post">
+        <div id="prihlasenieAdministratora">
+            <h2>Prihlásenie administrátora</h2>
+            <table style="width:100%;">
+                <tr>
+                    <td><label for="heslo">Heslo:</label></td>
+                    <td><input type=password name="heslo" id="heslo"></td>
+                </tr>
+                <?php
+                if (isset($_POST["heslo"])) {
+                    ?>
+                    <tr>
+                        <td></td>
+                        <td class="upozornenie">Ľutujeme, zadali ste zlé heslo</td>
+                    </tr>
+                <?php } ?>
+                <tr>
+                    <td>
+                        <div id="poslatHeslo" onclick="posli()">Zabudol som heslo</div>
+                    </td>
+                    <td><input type=submit name="prihlas" id="prihlas" value="Prihlásiť"></td>
+                </tr>
+            </table>
+        </div>
+    </form>
+    <?php
 }
-else{
-?>
-  <form method="post">
-    <div id="prihlasenieAdministratora">
-      <h2>Prihlásenie administrátora</h2>
-      <table style="width:100%;">
-        <tr>
-          <td><label for="heslo">Heslo:</label></td>
-          <td><input type=password name="heslo" id="heslo"></td>
-        </tr>
-        <?php
-        if(isset($_POST["heslo"])){?>
-          <tr>
-            <td></td>
-            <td class="upozornenie">Ľutujeme, zadali ste zlé heslo</td>
-          </tr>
-        <?php } ?>
-        <tr>
-          <td><div id="poslatHeslo" onclick="posli()">Zabudol som heslo</div></td>
-          <td><input type=submit name="prihlas" id="prihlas" value="Prihlásiť"></td>
-        </tr>
-      </table>
-    </div>
-</form>
-<?php
-}
-paticka();
-?>
-
+paticka(); ?>
 </html>
